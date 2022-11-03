d84f4d89fb057b1a1b31c5d388540b6b898ba124
Anasta81 <anastaabramova8@gmail.com>
2022-11-03 22:30:09 +0300

    3.1.2

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,27 @@
+package web.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "ability")
+    private String ability;
+
+    @Column(name = "alias")
+    private String alias;
+}
